d5855bc3fecfe46634bdc58d5a575e7f7de7b345
2angel1215 <2angel1215@gmail.com>
2026-04-04 10:39:16 +0900

    chore: feature/auth에서 모델 파일 분리

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,7 +1,3 @@
 from app.models.user import Base, User
-from app.models.gps_log import GpsLog
-from app.models.place import Place
-from app.models.daily_record import DailyRecord
-from app.models.photos import Photo
 
-__all__ = ["Base", "User", "GpsLog", "Place", "DailyRecord", "Photo"]
+__all__ = ["Base", "User"]
