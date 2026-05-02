62b375dabf4bcaef5f371902df83b449f654c61d
2angel1215 <2angel1215@gmail.com>
2026-05-02 18:55:08 +0900

    style: black 포맷 적용

--- backend/app/database.py
+++ backend/app/database.py
@@ -12,7 +12,6 @@ engine = create_async_engine(settings.DATABASE_URL, echo=True)
 async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
-
 async def get_db():
     async with async_session() as session:
         yield session
