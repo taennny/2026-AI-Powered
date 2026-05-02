67f7ae8801f5f59a444332b5a2a830320c693a2b
2angel1215 <2angel1215@gmail.com>
2026-05-02 18:43:09 +0900

    fix: dependencies.py status import 추가

--- backend/app/utils/dependencies.py
+++ backend/app/utils/dependencies.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
