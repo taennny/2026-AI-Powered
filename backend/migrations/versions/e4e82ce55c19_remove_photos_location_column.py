39aa9133b159444623c63bca6bfa3ad9aaac70b6
2angel1215 <2angel1215@gmail.com>
2026-06-20 13:07:59 +0900

    style: black formatting

--- backend/migrations/versions/e4e82ce55c19_remove_photos_location_column.py
+++ backend/migrations/versions/e4e82ce55c19_remove_photos_location_column.py
@@ -1,27 +1,29 @@
 """remove photos location column
+
 Revision ID: e4e82ce55c19
 Revises: 67aaac93cfa6
 Create Date: 2026-06-20 03:54:53.049570
+
 """
+
 from typing import Sequence, Union
+
 from alembic import op
 import sqlalchemy as sa
-from geoalchemy2 import Geometry
 
-revision: str = 'e4e82ce55c19'
-down_revision: Union[str, None] = '67aaac93cfa6'
+
+# revision identifiers, used by Alembic.
+revision: str = "e4e82ce55c19"
+down_revision: Union[str, None] = "67aaac93cfa6"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.drop_index("idx_photos_location", table_name="photos")
-    op.drop_column("photos", "location")
+    """Upgrade schema."""
+    pass
 
 
 def downgrade() -> None:
-    op.add_column(
-        "photos",
-        sa.Column("location", Geometry("POINT", srid=4326), nullable=True),
-    )
-    op.create_index("idx_photos_location", "photos", ["location"], postgresql_using="gist")
+    """Downgrade schema."""
+    pass
