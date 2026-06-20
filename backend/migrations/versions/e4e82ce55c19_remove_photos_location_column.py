6f41024e7595b13690a4a72d5f1c64ada0163202
2angel1215 <2angel1215@gmail.com>
2026-06-20 13:11:02 +0900

    style: black formatting migration file

--- backend/migrations/versions/e4e82ce55c19_remove_photos_location_column.py
+++ backend/migrations/versions/e4e82ce55c19_remove_photos_location_column.py
@@ -9,7 +9,6 @@ Create Date: 2026-06-20 03:54:53.049570
 from typing import Sequence, Union
 
 
-
 # revision identifiers, used by Alembic.
 revision: str = "e4e82ce55c19"
 down_revision: Union[str, None] = "67aaac93cfa6"
