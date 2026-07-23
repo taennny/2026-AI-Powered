697799596780c81f8e38ed544b8d6db70ae7ebb4
yeon-uu <dyu8765@gmail.com>
2026-07-23 18:52:27 +0900

    chore: SQL echo 설정화 (기본 off)
    - create_async_engine(echo=True) 하드코딩으로 프로덕션 로그가 SQL로 오염
    - SQL_ECHO 설정 추가, 기본 false

--- backend/app/database.py
+++ backend/app/database.py
@@ -8,7 +8,7 @@ class Base(DeclarativeBase):
     pass
 
 
-engine = create_async_engine(settings.DATABASE_URL, echo=True)
+engine = create_async_engine(settings.DATABASE_URL, echo=settings.SQL_ECHO)
 async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
